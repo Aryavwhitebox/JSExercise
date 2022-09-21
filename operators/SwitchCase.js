0f91dc1af440e3725fd350f81bf4d4595bc3d198
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-09-22 02:40:49 +0530

    Add files via upload

---
+++ operators/SwitchCase.js
@@ -0,0 +1,11 @@
+let a='monday'
+switch(a){
+    case 'monday':console.log('its monday'); break;
+    case 'tuesday':console.log('its tuesday'); break;
+    case 'wednesday':console.log('its wednesday'); break;
+    case 'thursday':console.log('its thursday'); break;
+    case 'friday':console.log('its friday'); break;
+    case 'saturday':console.log('its saturday'); break;
+    case 'sunday':console.log('its sunday'); break;
+default: console.log('doesnt have a week day');
+}
