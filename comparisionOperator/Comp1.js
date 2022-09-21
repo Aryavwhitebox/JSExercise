25793bf2942ec233afb57caed0f959ce873eb2fe
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-09-22 05:19:36 +0530

    Add files via upload

---
+++ comparisionOperator/Comp1.js
@@ -0,0 +1,24 @@
+if(5===5){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+if(5>10 && 10>5){
+    //even if one condition is false
+    //we will get false for and and
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+if(9>5 || 10>11){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+//ternary operator
+2<3 ?console.log(true) : console.log(false);
