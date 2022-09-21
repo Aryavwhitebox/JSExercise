9513203b991121534967b55540a5e37865d1c819
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-09-22 02:39:48 +0530

    Add files via upload

---
+++ conditional operators/Ifelse.js
@@ -0,0 +1,51 @@
+// if(5>7){
+//     console.log(true);
+// }
+// else if(7>8){
+//     console.log(false);
+// }
+
+// else{
+//     console.log("nothing");
+// }
+    //EXAMPLE 2
+// let length=200
+// let breadth=200
+// if(length==breadth){
+//     console.log('square');
+// }
+
+//     //EXAMPLE 3
+//     let num=5
+//     let num2=9
+//     let num3=20
+// if(num>num2 && num>num3){
+//     console.log('num is greatest');
+// }
+// else if(num2>num && num2>num3){
+//     console.log("num2 is greatest");
+// }
+// else{
+//     console.log(" num3 is greatest");
+// }
+
+let a=70
+if(a<25){
+    console.log('F');
+}
+else if(a>25 && a<45){
+    console.log('E');
+}
+
+else if(a>45 && a<50){
+    console.log('D');
+}
+else if(a>50 && a<60){
+    console.log('C');
+}
+else if(a>60 && a<80){
+    console.log('B');
+}
+else{
+    console.log('A');
+}
