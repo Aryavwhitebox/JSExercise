8f4a9a87cb7c3c9e1c8fe8b597a86ee88b543505
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-09-22 05:20:35 +0530

    Add files via upload

---
+++ Coercion/implicit.js
@@ -0,0 +1,16 @@
+let s='hello'
+let s1=5
+s1+=s
+console.log(s1);  //5hello
+console.log(typeof(s1)); //here the javascript itself convert
+//s1 which is int to a string
+
+if(''){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+//apart from 0 null and undefned all other values 
+//in the parameter of if treats as true
