3e4489b93741d7a61954715dfc562e05cafaf55a
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-02-14 13:15:09 -0800

    Update varletandconst.js

--- VarLetAndConst/varletandconst.js
+++ VarLetAndConst/varletandconst.js
@@ -5,7 +5,7 @@ var x=0
 var x=10
 console.log(x);
 
-var block variable can be redclared outside the block
+//var block variable can be redclared outside the block
 {
     var x=2
 }
@@ -13,13 +13,13 @@ var block variable can be redclared outside the block
 var x=3
 console.log(x);
 
-let can be accessed in the same block
-outside its block it cannot be accessed
+//let can be accessed in the same block
+//outside its block it cannot be accessed
 {
     let x=10
     //console.log(x);
 }
-here we will get an error console.log(x);
+//here we will get an error console.log(x);
 
 let x=10
 {
@@ -28,32 +28,32 @@ let x=10
 }
 console.log('outside the block '+x);
 
-lets try redclaring a var variable
+//lets try redclaring a var variable
 var x=5
 let x=10
 
-this is not allowed redclaring again using let console.log(x);
+//this is not allowed redclaring again using let console.log(x);
 
 
-CONST
+//CONST
 const h=10
 console.log(h);
-const values should be assigned when they are declared
+//const values should be assigned when they are declared
 const a=10
 a=10
 console.log(a);
 
 
-but we can change the values of an array
-You can create a constant array:
+//but we can change the values of an array
+//You can create a constant array:
 const cars = ["Saab", "Volvo", "BMW"];
 
-You can change an element:
+//You can change an element:
 cars[0] = "Toyota";
 cars.push("Audi");
 console.log(cars);
 
-lets try reassigning the values in array
+//lets try reassigning the values in array
 
 const cars = ["Saab", "Volvo", "BMW"];
 
