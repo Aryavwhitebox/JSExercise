a4a53993000be3fdbefaacdf8c81585251cef7a1
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-02-14 11:22:10 -0800

    Create switchcase.js

---
+++ Loops/switchcase.js
@@ -0,0 +1,10 @@
+ let a='monday'
+ switch(a){
+  case 'monday':console.log('day1');break
+    case 'tuesday':console.log('day2');break
+   case 'wednesday':console.log('day3');break
+    case 'thursday':console.log('day4');break
+    case 'friday':console.log('day5');break
+    case 'saturday':console.log('day6');break
+ default: console.log('not a week day');
+ }
