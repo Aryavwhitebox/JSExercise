03fb2f099460378b39ca9a2e0e1b979bc8176cb0
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-02-14 11:24:00 -0800

    Create ElseIfLadder.js

---
+++ Loops/ElseIfLadder.js
@@ -0,0 +1,13 @@
+let num1=1
+let num2=2
+let num3=3
+if(num1>num2&& num1>num3){
+    console.log('num1 is the greatest');
+}
+
+else if(num2>num1 && num2>num3){
+    console.log('num2 is greatest');
+}
+else{
+    console.log('num3 is greatest');
+}
